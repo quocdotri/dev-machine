27c3dae32e7fdfb5cc96114349459acd9b634641
quoc_dotri <quoc.dotri@coincheck.com>
2018-12-27 18:11:57 +0900

    add vimnox

--- site-cookbooks/vim-plugin/recipes/default.rb
+++ site-cookbooks/vim-plugin/recipes/default.rb
@@ -3,37 +3,45 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
-  node['vim_users'].each do |user|
-    directory "/home/#{user}/.vim" do
+#
+
+# for neocomplete https://github.com/Shougo/neocomplete.vim
+package 'vim-nox' do
+  action :install
+  retries 5
+end
+
+node['vim_users'].each do |user|
+  directory "/home/#{user}/.vim" do
+    owner user
+    group 'dev'
+    mode '0755'
+    action :create
+  end
+
+  %w(backup bundle swap).each do |dir|
+    directory "/home/#{user}/.vim/#{dir}" do
       owner user
       group 'dev'
       mode '0755'
       action :create
     end
+  end
 
-    %w(backup bundle swap).each do |dir|
-      directory "/home/#{user}/.vim/#{dir}" do
-        owner user
-        group 'dev'
-        mode '0755'
-        action :create
-      end
-    end
+  execute 'install neobundle' do
+    user user
+    group 'dev'
+    cwd "/home/#{user}"
+    command 'curl -L https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh'
+    environment 'HOME' => "/home/#{user}"
+    not_if { File.exists?("/home/#{user}/.vim/bundle/neobundle.vim") }
+  end
 
-    execute 'install neobundle' do
-      user user
+  %w(.vimrc).each do |file|
+    cookbook_file "/home/#{user}/#{file}" do
+      owner user
       group 'dev'
-      cwd "/home/#{user}"
-      command 'curl -L https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh'
-      environment 'HOME' => "/home/#{user}"
-      not_if { File.exists?("/home/#{user}/.vim/bundle/neobundle.vim") }
-    end
-
-    %w(.vimrc).each do |file|
-      cookbook_file "/home/#{user}/#{file}" do
-        owner user
-        group 'dev'
-        mode '0644'
-      end
+      mode '0644'
     end
   end
+end
